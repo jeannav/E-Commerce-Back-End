ccb766933ae48b0a6ffde571620d4552dae3a79d
jeannav <120337476+jeannav@users.noreply.github.com>
2023-05-05 16:55:33 -0700

    added code to models, and api routes, and README

---
+++ routes/api/category-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+// not sure if I am supposed to use await in line 10, b/c I did I had to add async to line 6
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// not sure if I am supposed to use await in line 21, b/c I did I had to add async to line 17
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with category, using the product through table
+      include: [{ model: Category, through: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// not sure if I am supposed to use await in line 39, b/c I did I had to add async to line 37
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// not sure if I am supposed to use await in line 48, b/c I did I had to add async to line 46
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// not sure if I am supposed to use await in line 52, b/c I did I had to add async to line 50
+  router.delete('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+module.exports = router;
